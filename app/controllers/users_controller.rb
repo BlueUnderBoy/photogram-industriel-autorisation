d00430417753cfa3b59f75e65fbb7a309d8a22b1
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2025-04-19 18:27:39 +0000

    corrige les fichiers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed discover ]
+  before_action :user_verification, only: [:feed, :discover]
 
   def index
     @users = @q.result
@@ -14,4 +15,11 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
+
+    def user_verification
+      if @user != current_user
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that. From Users Controller."
+      end
+    end
+
 end
